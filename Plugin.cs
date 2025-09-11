f3c0cf6fdfef900b0414aa3b0d4babb954abf7c6
Yuki-Codes <Yuki.Walsh@Outlook.com>
2025-09-11 20:24:16 +1000

    header

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,10 @@
-﻿// This software is licensed under the GNU AFFERO GENERAL PUBLIC LICENSE v3
+﻿// .______ _____ ___________   _______   ___   _ _____
+//  | ___ \  ___|  ___| ___ \ /  ___\ \ / / \ | /  __ \
+//  | |_/ / |__ | |__ | |_/ / \ `--. \ V /|  \| | /  \/
+//  |  __/|  __||  __||    /   `--. \ \ / | . ` | |
+//  | |   | |___| |___| |\ \  /\__/ / | | | |\  | \__/
+//  \_|   \____/\____/\_| \_| \____/  \_/ \_| \_/\____/
+//  This software is licensed under the GNU AFFERO GENERAL PUBLIC LICENSE v3
 
 namespace PeerSync;
 
